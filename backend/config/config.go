6f9f80126741b1d21fc11de4b2a149eee98850b5
gonpapa <naoki.0821g@gmail.com>
2023-02-18 17:27:08 +0900

    configパッケージの作成

---
+++ backend/config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"github.com/caarlos0/env/v6"
+)
+
+type Config struct {
+	Env      string `env:"APP_ENV" envDefault:"dev"`
+	Url      string `env:"APP_URL" envDefault:"http://localhost"`
+	Port     string `env:"PORT" envDefault:":8080"`
+	TimeZone string `env:"TIME_ZONE" envDefault:"Asia/Tokyo"`
+}
+
+// singleton
+func GetConfig() (*Config, error) {
+	cnf := &Config{}
+	if err := env.Parse(cnf); err != nil {
+		return nil, err
+	}
+
+	return cnf, nil
+}
