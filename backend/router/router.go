8c08ee3421de57e034f9b419497e2f3803aae48a
gonpapa <naoki.0821g@gmail.com>
2023-02-22 00:41:49 +0900

    アクセスログのミドルウェアを追加

--- backend/router/router.go
+++ backend/router/router.go
@@ -2,17 +2,35 @@ package router
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/gon-papa/record/util/logger"
+	"github.com/gon-papa/record/util/response"
 )
 
 func NewMux() http.Handler {
 	r := chi.NewRouter()
+	r.Use(accessLogger)
 
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"res": "hello"}`))
+		res := response.NewResponse("OK", nil)
+		res.CreateResponse(w, 200)
 
 	})
 	return r
+}
+
+func accessLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+
+		remoteAddr := r.RemoteAddr
+		// リバースプロキシなどからアクセスしている場合は元のIPを取得する
+		if forwardedFor := r.Header.Get("X-Forwarded-For"); forwardedFor != "" {
+			remoteAddr = forwardedFor
+		}
+		logger.AccessLog("ip", remoteAddr, "method", r.Method, "path", r.URL.Path, "proto", r.Proto, "host", r.Host, "time", time.Since(start))
+	})
 }
